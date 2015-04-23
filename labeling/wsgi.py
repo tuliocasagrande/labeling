ab4bbc6fc832003e1a4dc20081f71d5314c01378
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-04-22 21:53:50 -0300

    It works! Added application strutuctre working with apache + mod_wsgi

---
+++ labeling/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for labeling project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "labeling.settings"
+
+def application(environ, start_response):
+    for key in environ:
+        if key.startswith('LB_'):
+            os.environ[key] = environ[key]
+    return get_wsgi_application()(environ, start_response)
