5338f5145646726e367181eff347a08dad81a2ae
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-05-16 14:57:57 -0300

    Persisting hidden columns in the URL

--- app/static/app/js/label.js
+++ app/static/app/js/label.js
@@ -1,28 +1,68 @@
+function hideColumn(column, update_url) {
+  update_url = typeof update_url !== 'undefined' ? update_url : true;
+  $('#table-column-' + column + '-btn').fadeIn();
+  $('.table-column-' + column).fadeOut().promise().done(function() {
+    if (update_url) updateUrl();
+  });
+}
+
+function showColumn(column) {
+  $('#table-column-' + column + '-btn').fadeOut();
+  $('.table-column-' + column).fadeIn().promise().done(function() {
+    updateUrl();
+  });
+}
+
+function updateUrl() {
+  var $visible_columns = $('.show-column:visible');
+  var total = $visible_columns.length;
+  if (total === 0) {
+    window.history.replaceState({}, '', location.pathname);
+  } else {
+    var url = '?hidden-columns=';
+    $visible_columns.each(function(index) {
+      url += $(this).attr('data-column');
+      if (index !== total - 1) {
+        url += ',';
+      }
+    });
+    window.history.replaceState({}, '', url);
+  }
+}
+
+function parseURI(val) {
+  var result = '', tmp = [];
+  location.search.substr(1).split("&").forEach(function(item) {
+    tmp = item.split("=");
+    if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+  });
+  return result;
+}
+
 $(function () {
+  parseURI('hidden-columns').split(',').forEach(function(item) {
+    hideColumn(item, false);
+  });
+
   $('#body-container').removeClass('container').addClass('container-fluid');
 
   $('[data-toggle="tooltip"]').tooltip();
 
   $('.hide-column').click(function() {
-    var column = $(this).attr('data-column');
-    $('.' + column).fadeOut(500);
-    $('#' + column + '-btn').fadeIn(500);
+    hideColumn($(this).attr('data-column'));
   });
 
   $('.show-column').click(function() {
-    var column = $(this).attr('data-column');
-    $('.' + column).fadeIn(500);
-    $('#' + column + '-btn').fadeOut(500);
+    showColumn($(this).attr('data-column'));
   });
 
-  $('.label-btn').click(function() {
+  $('#samples_tbody').on('click', '.label-btn', function(e) {
     var $this = $(this);
     var label = $this.attr('data-label');
     var sample_id = $this.parent().attr('data-sample-id');
 
     $this.removeClass('btn-default').addClass($this.attr('btn-color'));
     $this.siblings().removeClass('btn-danger btn-warning btn-success btn-primary').addClass('btn-default');
-
 
     $.ajax({
       method: 'POST',
@@ -34,7 +74,7 @@ $(function () {
       console.log(data.responseText);
     })
     .done(function(data) {
-      console.log(data);
+      $this.parent().parent().fadeOut();
     });
   });
 
@@ -48,6 +88,10 @@ $(function () {
       console.log(data.responseText);
     })
     .done(function(data) {
+      $('#samples_tbody').append(data);
+      parseURI('hidden-columns').split(',').forEach(function(item) {
+        hideColumn(item, false);
+      });
       console.log(data);
     });
   });
