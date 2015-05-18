07644f70fa87efc4bcc88398ed68864218ca2bf0
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-05-18 10:24:22 -0300

    New dashboard.html

--- app/views.py
+++ app/views.py
@@ -10,8 +10,10 @@ def index(request):
   if request.user.is_authenticated():
     my_datasets = Dataset.objects.filter(owner=request.user.id)
     others_datasets = Contribution.objects.filter(contributor=request.user.id)
+    public_datasets = Dataset.objects.filter(privacy='public')
 
-    context = {'my_datasets': my_datasets, 'others_datasets': others_datasets}
+    context = {'my_datasets': my_datasets, 'others_datasets': others_datasets,
+               'public_datasets': public_datasets}
     return render(request, 'app/dashboard.html', context)
   else:
     return render(request, 'app/index.html')
